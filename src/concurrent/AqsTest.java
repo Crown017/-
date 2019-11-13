c71b99ed303f8707a81a0b24ad76b5f9d40a0c65
卢海辉 <luhaihui@sqhtech.com>
2019-11-13 14:54:56 +0800

    docs

--- src/concurrent/AqsTest.java
+++ src/concurrent/AqsTest.java
@@ -2,6 +2,10 @@ package concurrent;
 
 import java.util.concurrent.locks.ReentrantLock;
 
+
+/**
+ * 源码的入口
+ */
 public class AqsTest {
     private Object obj =  new Object();
     public static void main(String[] args) {
